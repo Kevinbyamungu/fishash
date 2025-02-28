2a6487d4b44985d7545aadedae1aefe47b516fc7
Kevinbyamungu <156521492+Kevinbyamungu@users.noreply.github.com>
2025-02-28 22:16:34 +0100

    page d'accueil
    page d'accueil

--- app/Controllers/PublicControllers.php
+++ app/Controllers/PublicControllers.php
@@ -8,11 +8,13 @@ use Psr\Container\ContainerInterface;
 
 class PublicControllers {
 
+    private $container;
+
     public function __construct(ContainerInterface $container) {
         $this->container = $container;
     }
 
-    public function home( Request $request, Response $response ): Response {
+    public function home( Request $request, Response $response ) {
         $views = $this->container->get('path')->run('public.home');
         $response->getBody()->write($views);
         return $response;
--- templates/public/home.blade.php
+++ templates/public/home.blade.php
@@ -7,7 +7,34 @@
     <title>Home</title>
     <script src="./assets/js/tailwindcss.js"></script>
 </head>
-<body>
-    
+<body class="bg-[#EEDDCC]">
+    <header class="shadow-md" >
+        <div class="container mx-auto flex justify-between items-center p-4" >
+            <h1 class="text-2x1 font-bold" >FIFASH</h1>
+            <nav>
+                <ul class="flex space-x-6">
+                    <li><a href="#" class="hover:text-gray-500" >HOME</a></li>
+                    <li><a href="#" class="hover:text-gray-500" >MEN</a></li>
+                    <li><a href="#" class="hover:text-gray-500" >WOMEN</a></li>
+                    <li><a href="#" class="hover:text-gray-500" >KIDS</a></li>
+                    <li><a href="#" class="hover:text-gray-500" >COLLECTION</a></li>
+                    <li><a href="#" class="hover:text-gray-500" >TRENDS</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section class="br-gradient-to-r from-gray-50 to-gray-200 py-16" >
+        <div class="container mx-auto flex items-center justify-between">
+            <div class="w-1/2 p-6">
+                <h2 class="text-5x1 font-bold mb-5 ">Find The Best Fashion Style For You</h2>
+                <p class="text-gray-600 mb-4">Découvrez les dernières tendances et styles qui vous correspondent.</p>
+                <a href="#"><button class="px-6 py-3 bg-black text-white font-semibold rounded-lg hover:bg-gray-800" >Shop Now</button></a>
+            </div>
+            <div class="w-1/2">
+                <img src="../../public/assets/photo/Rectangle 6.png" alt="" class="rounded-lg shadow-lg" >
+            </div>
+        </div>
+    </section>
 </body>
 </html>
